9a254ea25e327c30425f97e0d9903c381a9410fe
Rustymattok <mrArxi@gmail.com>
2020-04-07 15:07:13 +0300

    add  @Autowired for set

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -3,9 +3,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class StartUI {
-    @Autowired
+
     private Store store;
-    @Autowired
     private ConcoleInput cl;
 //    public StartUI(Store store,ConcoleInput cl) {
 //        this.store = store;
@@ -20,5 +19,13 @@ public class StartUI {
         for (String value : store.getAll()) {
             System.out.println(value);
         }
+    }
+    @Autowired
+    public void setStore(Store store) {
+        this.store = store;
+    }
+    @Autowired
+    public void setCl(ConcoleInput cl) {
+        this.cl = cl;
     }
 }
