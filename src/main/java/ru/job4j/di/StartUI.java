cd50d005006db7c3ed60bdb6ece4df1c874b3b18
Rustymattok <mrArxi@gmail.com>
2020-04-07 15:01:06 +0300

    add  @Autowired for constructor

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,16 +1,16 @@
 package ru.job4j.di;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 @Component
 public class StartUI {
-
+    @Autowired
     private Store store;
+    @Autowired
     private ConcoleInput cl;
-
-    public StartUI(Store store,ConcoleInput cl) {
-        this.store = store;
-        this.cl = cl;
-    }
-
+//    public StartUI(Store store,ConcoleInput cl) {
+//        this.store = store;
+//        this.cl = cl;
+//    }
     public void add(String value) {
         store.add(value);
     }
