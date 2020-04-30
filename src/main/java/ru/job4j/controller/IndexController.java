0af887f064d7d3c90bab39cbc27cd69da5fe9b2f
Rustymattok <mrArxi@gmail.com>
2020-04-30 16:27:42 +0300

    add support for UTF-8 (Russian letters)

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,14 +1,15 @@
 package ru.job4j.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.model.Accident;
 import ru.job4j.service.AccidentService;
 /**
  * Controller for police.jsp.
  */
 @Controller
-@RequestMapping("/police")
+@RequestMapping(value ="/police",produces = {"application/xml; charset=UTF-8"})
+@SessionAttributes(types = Accident.class)
 public class IndexController {
     @GetMapping
     public String showPolice(Model model){
