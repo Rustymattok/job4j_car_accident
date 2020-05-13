23157268b2b0c255b4394ed7bf39a204811188b2
Rustymattok <mrArxi@gmail.com>
2020-05-13 09:29:36 +0300

    add service

--- src/main/java/ru/job4j/service/AccidentService.java
+++ src/main/java/ru/job4j/service/AccidentService.java
@@ -1,26 +1,44 @@
 package ru.job4j.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.job4j.model.Accident;
-import ru.job4j.repository.AccidentRepository;
-import java.util.LinkedList;
+import ru.job4j.repository.AccidentRepo;
 import java.util.List;
 /**
  * Logic of application. (Data -> Spring connection)
  */
-public class AccidentService {
-    private static  final AccidentService INSTANCE = new AccidentService();
-
-    private AccidentService() {
+@Service
+public class AccidentService implements AccidentStore {
+    private final AccidentRepo accidentRepo;
+    @Autowired
+    public AccidentService(AccidentRepo accidentRepo) {
+        this.accidentRepo = accidentRepo;
     }
 
-    public static AccidentService getINSTANCE() {
-        return INSTANCE;
+    @Override
+    @Transactional
+    public void add(Accident accident) {
+        System.out.println("try to add?");
+        System.out.println(accident.getName());
+        accidentRepo.save(accident);
     }
 
-    public List<Accident> accidewtList(){
-        List<Accident> list = new LinkedList<>();
-        AccidentRepository accidentRepository = AccidentRepository.getINSTANCE();
-        list = accidentRepository.get();
-        System.out.println(list.size());
-        return list;
+    @Override
+    @Transactional
+    public List<Accident> get() {
+        return (List<Accident>) accidentRepo.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void remove(Integer id) {
+        accidentRepo.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public void update(Integer id) {
+      //todo реавлизовать доп метод в Crud.
     }
 }
