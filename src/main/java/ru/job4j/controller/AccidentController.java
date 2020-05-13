70b32984507013f76664c9b2e5ad27b6f7ce7037
Rustymattok <mrArxi@gmail.com>
2020-05-13 09:25:27 +0300

    update controller witn inject repo hibernate

--- src/main/java/ru/job4j/controller/AccidentController.java
+++ src/main/java/ru/job4j/controller/AccidentController.java
@@ -1,14 +1,22 @@
 package ru.job4j.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.job4j.model.Accident;
 import ru.job4j.repository.AccidentRepository;
+import ru.job4j.service.AccidentStore;
 
 @Controller
 @RequestMapping("/accident")
 @SessionAttributes(types = Accident.class)
 public class AccidentController {
+    private final AccidentStore accidentStore;
+    @Autowired
+    public AccidentController(AccidentStore accidentStore) {
+        this.accidentStore = accidentStore;
+    }
+
     @GetMapping
     public ModelAndView accidentPage(){
         ModelAndView model = new ModelAndView();
@@ -19,7 +27,8 @@ public class AccidentController {
 
     @PostMapping
     public String fillForm(@ModelAttribute("modelAccident")Accident accident){
-        AccidentRepository.getINSTANCE().add(accident);
+        accidentStore.add(accident);
+       // AccidentRepository.getINSTANCE().add(accident);
         return "redirect:/police";
     }
 
