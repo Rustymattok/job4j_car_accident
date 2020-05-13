3bf73e184dff30983e856508d0761c71a903a7c6
Rustymattok <mrArxi@gmail.com>
2020-05-13 09:25:48 +0300

    update controller witn inject repo hibernate

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,9 +1,12 @@
 package ru.job4j.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.model.Accident;
 import ru.job4j.service.AccidentService;
+import ru.job4j.service.AccidentStore;
+
 /**
  * Controller for police.jsp.
  */
@@ -11,10 +14,17 @@ import ru.job4j.service.AccidentService;
 @RequestMapping(value ="/police",produces = {"application/xml; charset=UTF-8"})
 @SessionAttributes(types = Accident.class)
 public class IndexController {
+    private final AccidentStore accidentStore;
+
+    @Autowired
+    public IndexController(AccidentStore accidentStore) {
+        this.accidentStore = accidentStore;
+    }
+
     @GetMapping
     public String showPolice(Model model){
         model.addAttribute("test","myText");
-        model.addAttribute("object", AccidentService.getINSTANCE().accidewtList());
+        model.addAttribute("object",accidentStore.get());
         return "police";
     }
 }
