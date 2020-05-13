25049a058f4bb3f6a972dba70d268e1ae1e86fa6
Rustymattok <mrArxi@gmail.com>
2020-05-13 09:29:07 +0300

    add service interface

--- src/main/java/ru/job4j/service/AccidentStore.java
+++ src/main/java/ru/job4j/service/AccidentStore.java
@@ -2,7 +2,7 @@ package ru.job4j.service;
 import ru.job4j.model.Accident;
 import java.util.List;
 /**
- * Common inteface for DataBase.
+ * Common inteface for DataBase. For Crud.
  */
 public interface AccidentStore {
     void add(Accident accident);
